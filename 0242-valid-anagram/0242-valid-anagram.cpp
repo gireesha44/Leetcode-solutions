ec8926dbcb3128d9706bfb29c1fe8aa43d81769b
gireesha44 <gireesha4444@gmail.com>
2026-06-29 17:48:28 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(char c:s){
+            mp1[c]++;
+        }
+        for(char c:t){
+            mp2[c]++;
+        }
+
+        return mp1==mp2;
     }
 };
