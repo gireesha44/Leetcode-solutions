7b55520f9972a40222dacc4debdac0c5ed752b05
gireesha44 <gireesha4444@gmail.com>
2026-06-29 16:08:37 +0530

    Time: 4 ms (22.52%), Space: 9.9 MB (95.34%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
+    
+        while(s[0]==' ')s.erase(0,1);
+        while(s[s.size()-1]==' ')s.erase(s.size()-1,1);
+        reverse(s.begin(),s.end());
         int n = s.size();
-        int i=0,j=n-1;
-        string ans = "";
-        while(s[i]==' ')i++;
-        while(s[j]==' ')j--;
-        string str="";
-        while(i<=j){
-            if(s[j]==' '){
-                while(s[j]==' ')j--;
-                reverse(str.begin(),str.end());
-                ans+=str+' ';
-                str="";
+        int i = 0,start=0,end= 0;
+        while(i<n){
+            start = i;
+            while(i<n && s[i]!=' '){
+                end = i;
+                i++;
             }
-            str+=s[j];
-            j--;
+            reverse(s.begin()+start,s.begin()+end+1);
+            while(i<n-2 && s[i]==' ' && s[i+1]==' ')i++;
+            if(s[i]==' ')i++;
         }
-        reverse(str.begin(),str.end());
-        ans+=str;
-        return ans;
+        i=0;
+        while(i<n){
+            while(s[i]==' ' && s[i+1]==' '){
+                s.erase(i,1);
+                n--;
+            }
+            i++;
+        }
+        return s;
     }
 };
