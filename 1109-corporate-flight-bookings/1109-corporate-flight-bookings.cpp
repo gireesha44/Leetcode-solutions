1b07b49a850fff050077c4670da1d9473592d9eb
gireesha44 <gireesha4444@gmail.com>
2026-06-29 19:42:31 +0530

    Time: 300 ms (16.44%), Space: 72.1 MB (89.86%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int>diff(n,0);
+        for(int i=0;i<bookings.size();i++){
+            diff[bookings[i][0]-1]+=bookings[i][2];
+            if(bookings[i][1]<n)diff[bookings[i][1]]-=bookings[i][2];
+        }
+        for(int x:diff)cout<<x<<" ";
+        for(int i=1;i<n;i++){
+            diff[i]+=diff[i-1];
+        }
+        return diff;
+    }
+};
