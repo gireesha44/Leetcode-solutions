5662ed1c0981f29f964962c10415ce343d06a29f
gireesha44 <gireesha4444@gmail.com>
2026-07-12 18:21:20 +0530

    Time: 5 ms (47.76%), Space: 16.8 MB (5.44%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,16 +1,60 @@
 class Solution {
 public:
+    vector<int> nege(vector<int>nums){
+        stack<int>st;
+        int n = nums.size();
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i])st.pop();
+            ans[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> prge(vector<int>nums){
+        stack<int>st;
+        int n = nums.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i])st.pop();
+            ans[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> nese(vector<int>nums){
+        stack<int>st;
+        int n = nums.size();
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i])st.pop();
+            ans[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> prse(vector<int>nums){
+        stack<int>st;
+        int n = nums.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>nums[i])st.pop();
+            ans[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return ans;
+    }
     long long subArrayRanges(vector<int>& nums) {
         int n =nums.size();
-        long long sum = 0;
+        long long minsum = 0,maxsum=0;
+        vector<int>pse = prse(nums);
+        vector<int>nse = nese(nums);
+        vector<int>nge = nege(nums);
+        vector<int>pge = prge(nums);
         for(int i=0;i<n;i++){
-            int maxi=INT_MIN,mini=INT_MAX;
-            for(int j=i;j<n;j++){
-                maxi = max(maxi,nums[j]);
-                mini = min(mini,nums[j]);
-                sum+=(maxi-mini);
-            }
+            minsum+=1LL*nums[i]*(i-pse[i])*(nse[i]-i);
+            maxsum+=1LL*nums[i]*(i-pge[i])*(nge[i]-i);
         }
-        return sum;
+        return maxsum-minsum;
     }
 };
