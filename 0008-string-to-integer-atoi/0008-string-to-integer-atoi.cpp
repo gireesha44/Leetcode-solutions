ec545d8e71154be8797cf5c02884d67d4e8d2841
gireesha44 <gireesha4444@gmail.com>
2026-07-19 21:37:57 +0530

    Time: 3 ms (13.76%), Space: 9 MB (82.28%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,26 +2,36 @@ class Solution {
 public:
     int myAtoi(string s) {
         int n = s.size();
+        int i=0;
+        while(s[i]==' ')i++;
+        bool sign = false,neg=false,overflow=false;
         long long ans = 0;
-        bool neg= false;
-        int f = 0,j=0;
-        while(s[j]==' ')j++;
-        if(s[j]=='-'){
-            neg=true;
-            j++;
-        }    
-        else if(s[j]=='+'){
-            neg = false;
-            j++;
+        for(int j=i;j<n;j++){
+            if((s[j]=='-' || s[j]=='+') && !sign){
+                if(ans==0){
+                    sign= true;
+                    if(s[j]=='-')neg = true;
+                }    
+                else break;
+            }
+            else if(s[j]>='0' && s[j]<='9'){
+                sign = true;
+                if(ans*10+(s[j]-'0')<=INT_MAX)ans = (ans*10)+(s[j]-'0');
+                else{
+                    overflow=true;
+                    break;
+                } 
+            }
+            else break;
         }
-        int i = j;
-        while(i<n && s[i]>='0' && s[i]<='9'){
-            ans = ans*10+(s[i]-'0');
-            if(neg && -ans<INT_MIN)return INT_MIN;
-            if(!neg && ans>INT_MAX)return INT_MAX;
-            
-            i++;
+        cout<<overflow;
+        if(sign && neg){
+            if(overflow)ans = INT_MIN;
+            else ans=-ans;
         }
-        return neg?-ans:ans;
+        else {
+            if(overflow)ans= INT_MAX;
+        }
+        return ans;
     }
 };
