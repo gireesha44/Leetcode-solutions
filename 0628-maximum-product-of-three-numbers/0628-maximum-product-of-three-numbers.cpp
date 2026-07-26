1339e8dff1e9f763cb3e0901b4c55a89ab81d026
gireesha44 <gireesha4444@gmail.com>
2026-07-26 10:24:55 +0530

    Time: 0 ms (100%), Space: 31.6 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,8 +1,48 @@
-class Solution {
-public:
-    int maximumProduct(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        return max({nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]});
-    }
+class Solution { 
+public: 
+    int maximumProduct(vector<int>& nums) { 
+        int n = nums.size(); 
+        
+        long long first_max = LLONG_MIN; 
+        long long second_max = LLONG_MIN; 
+        long long third_max = LLONG_MIN; 
+        long long first_min = LLONG_MAX; 
+        long long second_min = LLONG_MAX; 
+        
+        int f_max_idx = -1, s_max_idx = -1, t_max_idx = -1;
+        int f_min_idx = -1, s_min_idx = -1;
+
+        for(int i=0; i<n; i++){ 
+            if(first_max < nums[i]) {
+                first_max = nums[i]; 
+                f_max_idx = i;
+            }
+        } 
+        
+        for(int i=0; i<n; i++){ 
+            if(i != f_max_idx && second_max < nums[i]) {
+                second_max = nums[i]; 
+                s_max_idx = i;
+            }
+        } 
+    
+        for(int i=0; i<n; i++){ 
+            if(i != f_max_idx && i != s_max_idx && third_max < nums[i]) {
+                third_max = nums[i]; 
+            }
+        } 
+        
+        for(int i=0; i<n; i++){ 
+            if(first_min > nums[i]) {
+                first_min = nums[i]; 
+                f_min_idx = i;
+            }
+        } 
+        for(int i=0; i<n; i++){ 
+            if(i != f_min_idx && second_min > nums[i]) {
+                second_min = nums[i]; 
+            }
+        } 
+        return max({1LL * first_max * second_max * third_max, 1LL * first_min * second_min * first_max}); 
+    } 
 };
