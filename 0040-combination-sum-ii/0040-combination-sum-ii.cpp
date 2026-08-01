989b8cf2de6525f95fcd70502b23d29b8b26d25a
gireesha44 <gireesha4444@gmail.com>
2026-08-01 16:50:37 +0530

    Time: 3 ms (37.63%), Space: 14.1 MB (65.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     void generate(vector<vector<int>>& ans, vector<int>& arr, int k, int n, int ind, vector<int>& ds, int sum){
-        if (sum == k) {
+        if(ind==n || sum>k){
+            if(sum==k)ans.push_back(ds);
+            return;
+        }
+        if(sum==k){
             ans.push_back(ds);
             return;
         }
-        if (ind == n || sum > k) {
-            return;
-        }
         ds.push_back(arr[ind]);
-        generate(ans, arr, k, n, ind + 1, ds, sum + arr[ind]);
+        generate(ans,arr,k,n,ind+1,ds,sum+arr[ind]);
         ds.pop_back();
 
-        while (ind + 1 < n && arr[ind] == arr[ind + 1]) {
+        while(ind+1<n && arr[ind]==arr[ind+1]) {
             ind++;
         }
-        generate(ans, arr, k, n, ind + 1, ds, sum);
+        generate(ans,arr,k,n,ind+1,ds,sum);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
@@ -23,7 +24,7 @@ public:
         vector<vector<int>> ans;
         vector<int>ds;
         sort(arr.begin(), arr.end());
-        generate(ans, arr, target, n, 0, ds, 0);
+        generate(ans,arr,target,n,0,ds,0);
         return ans;
     }
 };
