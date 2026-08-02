2fb8cf319605b57fdb449f0ee8aa5d78accc04a9
gireesha44 <gireesha4444@gmail.com>
2026-08-02 18:43:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,14 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(s.begin(),s.end());
         sort(g.begin(),g.end());
-        int cnt = 0,i=0,j=0;
+        int i=0,j=0;
         while(i<s.size() && j<g.size()){
             if(g[j]<=s[i]){
-                cnt++;
                 i++;
             }
             j++;
         }
-        return cnt;
+        return i;
     }
 };
