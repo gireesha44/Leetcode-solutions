002c2f5c733826f224f85853754e48c01b496cdc
gireesha44 <gireesha4444@gmail.com>
2026-08-29 00:27:36 +0530

    Time: 15 ms (7.13%), Space: 20 MB (5.29%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -2,38 +2,27 @@ class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
         int n = words.size();
-        unordered_map<string, int> mp;
-
-        for (int i = 0; i < 26; i++) {
-
-            string c = string(1, 'a' + i);
-
-            int minFreq = INT_MAX;
-
-            for (string x : words) {
-
+        vector<int>present(26,0);
+        vector<int>min_freq(26,INT_MAX);
+        for(char c='a';c<='z';c++){
+            for(string word:words){
                 int freq = 0;
-
-                for (char ch : x) {
-                    if (ch == c[0]) {
-                        freq++;
-                    }
+                for(char ch:word){
+                    if(ch==c)freq++;
                 }
+                if(freq>0)present[c-'a']++;
 
-                minFreq = min(minFreq, freq);
-            }
-
-            mp[c] = minFreq;
-        }
-
-        vector<string> ans;
-
-        for (auto &p : mp) {
-            while (p.second--) {
-                ans.push_back(p.first);
+                min_freq[c-'a'] = min(min_freq[c-'a'],freq);
             }
         }
-
+        vector<string>ans;
+        for(int i=0;i<26;i++){
+            if(present[i]==n){
+                for(int j=0;j<min_freq[i];j++){
+                    ans.push_back(string(1,'a'+i));
+                }
+            }
+        }
         return ans;
     }
 };
